9e11fb9645e7be7d18875446535c2b4b061c3c18
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2022-10-31 18:40:33 +0100

    findNextSquare Java

--- Find the next perfect square!/findNextSquare.java
+++ Find the next perfect square!/findNextSquare.java
@@ -2,6 +2,6 @@ public class NumberFun
 {
 	public static long findNextSquare(long sq)
 	{
-		return (Math.round(Math.sqrt(sq)) == Math.sqrt(sq)) ? (long) (Math.sqrt(sq) + 1) * (long) (Math.sqrt(sq) + 1) : -1;
+		return (Math.round(Math.sqrt(sq)) == Math.sqrt(sq)) ? (long) Math.pow((Math.sqrt(sq) + 1), 2) : -1;
 	}
 }
